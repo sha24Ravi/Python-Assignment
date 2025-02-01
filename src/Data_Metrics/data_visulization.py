48be347e3d9ca53f198b6725ed91dae8d6c169d6
sha24Ravi <shashankr2322@gmail.com>
2025-02-01 17:15:44 -0500

    update

--- src/Data_Metrics/data_visulization.py
+++ src/Data_Metrics/data_visulization.py
@@ -4,7 +4,6 @@ from dataclasses import dataclass
 import streamlit as st
 import matplotlib.pyplot as plt
 import matplotlib
-import tkinter as tk
 matplotlib.use('TkAgg')
 from src.exception import CustomeException
 from src.loggers import logging
